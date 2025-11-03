ec37ae4dd17b8d40b009877555b35eaa8dd17868
yousef elsayed <yousef4511@outlook.com>
2025-11-03 12:35:00 +0200

    feat(day1): add command to create new todo items

--- day1/todo-cli/src/todo.js
+++ day1/todo-cli/src/todo.js
@@ -5,6 +5,14 @@ const program = new Command();
 
 program.name("todo-cli").description("A simple CLI tool for managing todos").version("1.0.0")
 
+program.command("add")
+  .alias("new")
+  .description("Add a new todo item")
+  .argument("<title>", "The task title")
+  .action((title) => {
+    console.log(`Added todo: ${title}`);
+  });
+
 program.parse();
 
 if (process.argv.length === 2) {
